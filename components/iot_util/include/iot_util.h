3ae5fa3b705a95143a54b2f8478cd976e7d4914c
Teiyem <teethedev@gmail.com>
2023-04-29 01:50:29 +0200

    Removed unused method

--- components/iot_util/include/iot_util.h
+++ components/iot_util/include/iot_util.h
@@ -11,5 +11,3 @@ void *allocate_mem(size_t size);
 char *concat_with_delimiter(const char *str1, const char *str2, const char *delimiter);
 esp_err_t split_with_delimiter(const char *str, const char *delimiter, char **str1, char **str2);
 esp_err_t check_string_validity(const char *str);
-
-unsigned long IRAM_ATTR millis();
