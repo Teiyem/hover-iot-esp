f002b618357917195a5bd16ab4f1cc44824daa18
Teiyem <teethedev@gmail.com>
2023-04-29 05:31:44 +0200

    Added error handling and updated server code

--- components/iot_server/include/iot_server.h
+++ components/iot_server/include/iot_server.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include "esp_http_server.h"
+#include "iot_util.h"
 
 /**
  * A class for handling networking with an HTTP server.
@@ -13,9 +14,13 @@ class iot_server
 public:
     iot_server(void);
     ~iot_server(void);
-    void register_route(const char *path, httpd_method_t method, std::function<esp_err_t(httpd_req_t *)> handler);
+    esp_err_t start(void);
+    esp_err_t validate_auth(httpd_req_t *req);
+    esp_err_t register_route(const char *path, httpd_method_t method, std::function<esp_err_t(httpd_req_t *)> handler);
 
 private:
+    static constexpr const char *tag = "iot_server";                         /** A constant used to identify the source of the log message of this class. */
+    static constexpr const char *_api_key = "aesY}zeN]v4DOp@o2)-";           /** Temporay api key*/
     httpd_handle_t _server;                                                  /** The handle to the HTTP server instance. */
     std::map<const char *, std::function<esp_err_t(httpd_req_t *)>> _routes; /** The handle to the HTTP server instance. */
     esp_err_t handle_route(httpd_req_t *req);
--- components/iot_server/iot_server.cpp
+++ components/iot_server/iot_server.cpp
@@ -5,9 +5,7 @@
  */
 iot_server::iot_server(void)
 {
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     _server = nullptr;
-    httpd_start(&_server, &config);
 }
 
 /**
@@ -22,13 +20,92 @@ iot_server::~iot_server(void)
 }
 
 /**
+ * Starts the http server.
+ * @returns ESP_OK on success, ESP_FAIL on failure.
+ */
+esp_err_t iot_server::start(void)
+{
+    esp_err_t ret = ESP_FAIL;
+
+    ESP_LOGI(tag, "%s -> Starting the device http server", __func__);
+
+    if (_server != nullptr)
+    {
+        ESP_LOGI(tag, "%s -> Device http server is already started", __func__);
+        return ret;
+    }
+
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    if (httpd_start(&_server, &config) != ESP_OK)
+    {
+        ESP_LOGI(tag, "%s -> Failed to start http server", __func__);
+        return ret;
+    }
+
+    return ESP_OK;
+}
+
+/**
+ * Checks the request header for authorization headers and a valid api-key.
+ * @param req The request object.
+ * @return An esp_err_t result of the.
+ * @return ESP_OK on success, ESP_FAIL request is not authorized.
+ */
+esp_err_t iot_server::validate_auth(httpd_req_t *req)
+{
+    ESP_LOGI(tag, "%s -> Verifying if the request is authorized", __func__);
+
+    const char *header = "x-api-key";
+
+    esp_err_t ret = ESP_FAIL;
+
+    auto header_len = httpd_req_get_hdr_value_len(req, header);
+
+    if (header_len < 1)
+    {
+        ESP_LOGE(tag, "%s -> Couldn't find the -> %s header", __func__, header);
+        return ret;
+    }
+
+    header_len += 1;
+
+    ESP_LOGI(tag, "%s -> Found -> %s header", __func__, header);
+
+    auto *api_key = static_cast<char *>(allocate_mem(header_len));
+
+    if (api_key == nullptr)
+    {
+        return ret;
+    }
+
+    ESP_LOGI(tag, "%s -> %s Header value retrieved, verifying match", __func__, header);
+
+    ret = strcmp(_api_key, api_key) == 0 ? ESP_OK : ESP_FAIL;
+
+    ESP_LOGI(tag, "%s -> Request is -> %s", __func__, ret == ESP_OK ? "authorized" : "unauthorized");
+
+    free(api_key);
+
+    return ret;
+}
+
+/**
  *  Register an HTTP route with the server.
  *
  * @param path The URL path to register.
  * @param handler The function to handle requests to the path.
+ * @returns ESP_OK on success, ESP_FAIL or ESP_ERR_INVALID_ARG on failure.
  */
-void iot_server::register_route(const char *path, httpd_method_t method, std::function<esp_err_t(httpd_req_t *)> handler)
+esp_err_t iot_server::register_route(const char *path, httpd_method_t method, std::function<esp_err_t(httpd_req_t *)> handler)
 {
+    ESP_LOGI(tag, "%s -> Registering route", __func__);
+
+    if (check_string_validity(path) != ESP_OK)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
     const httpd_uri_t uri_handler = {
         .uri = path,
         .method = method,
@@ -39,9 +116,19 @@ void iot_server::register_route(const char *path, httpd_method_t method, std::fu
         },
         .user_ctx = this};
 
-    httpd_register_uri_handler(_server, &uri_handler);
+    esp_err_t ret = httpd_register_uri_handler(_server, &uri_handler);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGI(tag, "%s -> Failed to register route -> %s", __func__, path);
+        return ret;
+    }
 
     _routes[path] = handler;
+
+    ESP_LOGI(tag, "%s -> Successfully registered route -> %s", __func__, path);
+
+    return ret;
 }
 
 /**
